55434d6e9b04c23fac10e2d11ad19e34994f879d
hoangnv <hoangnvd00536@fpt.edu.vn>
2021-09-28 15:39:49 +0700

    test1

--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::get('/seed', function () {
-//    $raw = file_get_contents(storage_path()."\app\public\plant_pathogen.txt");
-//    $datas = json_decode($raw,true);
-//    foreach ($datas["pathogen-list"] as $data){
-//        $res = \App\Models\PlantPathogen::create([
-//            'plant_id' => $data["id"],
-//            'name' => $data["name"],
-//            'name_en' => $data["name_en"],
-//            'pathogen_class' => $data["pathogen_class"],
-//            'scientific_name' => $data["scientific_name"],
-//        ]);
-//    }
-//});
+Route::get('/seed', function () {
+    $raw = file_get_contents(storage_path()."\app\public\plant_pathogen.txt");
+    $datas = json_decode($raw,true);
+    foreach ($datas["pathogen-list"] as $data){
+        $res = \App\Models\PlantPathogen::create([
+            'plant_id' => $data["id"],
+            'name' => $data["name"],
+            'name_en' => $data["name_en"],
+            'pathogen_class' => $data["pathogen_class"],
+            'scientific_name' => $data["scientific_name"],
+        ]);
+    }
+});
 
 Route::prefix("plant")->group(function () {
     Route::get("", [PlantController::class, 'index']);
